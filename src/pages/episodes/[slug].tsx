e9753e6f020a6940fcdb7cae58b59c53c27ecacb
Walison Miranda <46504580+WalisonMiranda@users.noreply.github.com>
2021-04-30 17:00:46 -0300

    Update [slug].tsx

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -70,50 +70,31 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await api.get("episodes", {
-    params: {
-      _limit: 12,
-      _sort: "published_at",
-      _order: "desc",
-    }
-  });
-
-  const paths = data.map(episode => ({
-    params: {
-      slug: episode.id,
-    }
-  }));
-
   return {
-    paths,
-    fallback: "blocking",
-  };
-};
+    paths: [],
+    fallback: 'blocking'
+  }
+}
 
-// context
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { slug } = ctx.params;
+  const { slug } = ctx.params
 
-  const { data } = await api.get(`/episodes/${slug}`);
+  const { data } = await api.get(`/episodes/${slug}`)
 
   const episode = {
     id: data.id,
     title: data.title,
     thumbnail: data.thumbnail,
     members: data.members,
-    publishedAt: format(parseISO(data.published_at), "d MMM yy", {
-      locale: ptBR,
-    }),
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {locale: ptBR}),
     duration: Number(data.file.duration),
     durationAsString: convertDurationToTimeString(Number(data.file.duration)),
     description: data.description,
     url: data.file.url,
-  };
+  }
 
   return {
-    props: {
-      episode,
-    },
-    revalidate: 60 * 60 * 24, // 24 hours
-  };
-};
+    props: { episode, },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
+}
